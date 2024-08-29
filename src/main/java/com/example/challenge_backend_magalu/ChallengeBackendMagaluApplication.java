4d347de0a1e778f1b19091e3e2f0e9b8967c4e59
caio <caiothalles607@gmail.com>
2024-08-29 08:58:08 -0300

    feat: initial commit

---
+++ src/main/java/com/example/challenge_backend_magalu/ChallengeBackendMagaluApplication.java
@@ -0,0 +1,13 @@
+package com.example.challenge_backend_magalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBackendMagaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBackendMagaluApplication.class, args);
+	}
+
+}
