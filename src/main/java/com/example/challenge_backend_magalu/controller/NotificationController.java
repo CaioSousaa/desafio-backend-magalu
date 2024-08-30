d578bcf75812c257aed633a623a539c3fc908f43
caio <caiothalles607@gmail.com>
2024-08-30 19:11:11 -0300

    chore: created endpoint findById

--- src/main/java/com/example/challenge_backend_magalu/controller/NotificationController.java
+++ src/main/java/com/example/challenge_backend_magalu/controller/NotificationController.java
@@ -1,29 +1,36 @@
 package com.example.challenge_backend_magalu.controller;
 
 import com.example.challenge_backend_magalu.controller.dto.ScheduleNotification;
+import com.example.challenge_backend_magalu.entity.Notification;
 import com.example.challenge_backend_magalu.service.NotificationService;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/notifications")
 @AllArgsConstructor
-@NoArgsConstructor
 public class NotificationController {
 
     @Autowired
-    private NotificationService notificationService;
+    private final NotificationService notificationService;
 
     @PostMapping
     public ResponseEntity<Void> scheduleNotification(@RequestBody ScheduleNotification dto) {
             notificationService.scheduleNotification(dto);
 
             return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping("/{notificationId}")
+    public ResponseEntity<Notification> getNotification(@PathVariable("notificationId") Long notificationId) {
+        var notification = notificationService.findById(notificationId);
+
+        if(notification.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+         return ResponseEntity.ok(notification.get());
     }
 }
--- src/main/java/com/example/challenge_backend_magalu/service/NotificationService.java
+++ src/main/java/com/example/challenge_backend_magalu/service/NotificationService.java
@@ -1,9 +1,12 @@
 package com.example.challenge_backend_magalu.service;
 
 import com.example.challenge_backend_magalu.controller.dto.ScheduleNotification;
+import com.example.challenge_backend_magalu.entity.Notification;
 import com.example.challenge_backend_magalu.repository.NotificationRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -12,6 +15,10 @@ public class NotificationService {
 
     public void scheduleNotification(ScheduleNotification scheduleNotifications) {
         notificationRepository.save(scheduleNotifications.toNotification());
+    }
+
+    public Optional<Notification> findById(Long notificationId) {
+        return notificationRepository.findById(notificationId);
     }
 
 }
