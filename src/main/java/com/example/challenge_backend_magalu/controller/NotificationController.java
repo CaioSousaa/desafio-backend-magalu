e84affd351948fe3338b6bd3ad4fbb285076346e
caio <caiothalles607@gmail.com>
2024-08-30 18:37:10 -0300

    chore: fixing a bug due to lack of a decorator

--- src/main/java/com/example/challenge_backend_magalu/controller/NotificationController.java
+++ src/main/java/com/example/challenge_backend_magalu/controller/NotificationController.java
@@ -4,6 +4,7 @@ import com.example.challenge_backend_magalu.controller.dto.ScheduleNotification;
 import com.example.challenge_backend_magalu.service.NotificationService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @NoArgsConstructor
 public class NotificationController {
 
+    @Autowired
     private NotificationService notificationService;
 
     @PostMapping
